c714019f48d3e14e03305ed7bde4f52ccf832da5
Trung <vn01122003@gmail.com>
2025-01-24 23:48:55 +0700

    new endpoint tutorial

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from typing import Optional
 from pydantic import BaseModel
 
@@ -23,3 +23,17 @@ async def create_book(book_data:BookCreateModel):
         "tittle":book_data.tittle,
         "author":book_data.author
     }
+
+@app.get('/get_headers',status_code=201)
+async def get_headers(
+    accept: str = Header(None),
+    content_type: str = Header(None),
+    user_agent: str = Header(None),
+    host: str = Header(None)
+):
+    request_header ={}
+    request_header["Accept"] = accept
+    request_header["Content-Type"] = content_type
+    request_header["User-Agent"] = user_agent
+    request_header["Host"] = host
+    return request_header

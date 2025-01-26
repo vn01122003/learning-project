c9dc8fe73af1a2e4e6d91cfb7819ba42620e5469
Trung <vn01122003@gmail.com>
2025-01-27 01:33:24 +0700

    restructure the pj for learning and ready for module part

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from src.book.routes import book_router
+
+version = "v1"
+app = FastAPI(
+    version=version
+)
+
+app.include_router(book_router,prefix=f"/api/{version}/books")

835be7616cc5f7b38decbfedf91b27e0ded9a0a5
Trung <vn01122003@gmail.com>
2025-02-04 18:57:26 +0700

    restrure the routers & requirement

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,9 @@ from src.book.routes import book_router
 
 version = "v1"
 app = FastAPI(
-    version=version
+    title="Book_Manager",
+    description="A REST API for manage book",
+    version=version,
 )
 
-app.include_router(book_router,prefix=f"/api/{version}/books")
+app.include_router(book_router,prefix=f"/api/{version}/books", tags=['books'])

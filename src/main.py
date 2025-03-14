75d0fed5811b05d71a27e2350eb01482a35b04c1
Thi <lnbaothi2411@gmail.com>
2025-03-14 10:53:54 +0700

    add function

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print ("Hello world")
+# print ("Hello world")
+# a = 100
+# b = 200
+# c = 300
+# print(a, b, c, sep = '--', end = '#')
+
+def helloWorld(a):
+    a = a + 100
+    b = a+11
+    return a,b
+a, _ = helloWorld(22)
+print(a)
